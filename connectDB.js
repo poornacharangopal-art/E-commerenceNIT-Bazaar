81da869b39b523761ffb2d75bd70e04599571996
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-20 22:38:44 +0530

    Improve MongoDB connection handling
    Added a connection state check to prevent multiple connections.

--- connectDB.js
+++ connectDB.js
@@ -1,9 +1,17 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 
+let isConnected = false;
+
 const connectDB = async () => {
+    if (isConnected) {
+        return;
+    }
+
     try {
         await mongoose.connect(process.env.MONGO_URI);
+
+        isConnected = true;
 
         console.log("MongoDB Connected");
         console.log("Ready State:", mongoose.connection.readyState);
