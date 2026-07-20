696421fd00261dcb7cc2421c41aab792fc4032b6
Poornacharangopal <poornacharangopal@gmail.com>
2026-07-20 11:21:50 +0530

    Enhance product schema with required fields
    Added 'Description' field with default value to product schema.

--- models/Product.js
+++ models/Product.js
@@ -1,12 +1,42 @@
 const mongoose=require("mongoose");
 
 const productSchema=new mongoose.Schema({
-    Name:String,
-    ImageUrl:String,
-    Cost:Number,
-    UserEmail:String,
-    UserName:String,
-    College:String
-});
+
+    Name:{
+        type:String,
+        required:true
+    },
+
+    ImageUrl:{
+        type:String,
+        required:true
+    },
+
+    Cost:{
+        type:Number,
+        required:true
+    },
+
+    Description:{
+        type:String,
+        default:""
+    },
+
+    UserEmail:{
+        type:String,
+        required:true
+    },
+
+    UserName:{
+        type:String,
+        required:true
+    },
+
+    College:{
+        type:String,
+        required:true
+    }
+
+},{timestamps:true});
 
 module.exports=mongoose.model("Product",productSchema);
